30397cd48469fea7e32e6dd335f1210d3c3d23bc
Kelo2000 <ltl2113@nyu.edu>
2023-07-03 14:33:30 -0400

    Adding navigation and Chat screen

--- front-end/Screens/Chat.js
+++ front-end/Screens/Chat.js
@@ -1,11 +1,94 @@
-import React from 'react';
+import React, {useState, useEffect,useCallback} from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
+import {FlatList, NativeBaseProvider } from "native-base"
+import { GiftedChat, Bubble, Send } from 'react-native-gifted-chat'
+import MaterialCommunity from 'react-native-vector-icons/MaterialCommunityIcons'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
 
 export default function Chat() {
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello Man!!',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    )
+  }, [])
+
+
+  const renderMyBubble=(props)=>{
+    return(
+    <Bubble
+    {...props}
+      wrapperStyle={{
+        right: {backgroundColor: '#9d4edd'},
+        left: {backgroundColor: '#9d4edd'},
+      }}
+
+      textStyle={{
+        right: {
+          color: '#fff'
+        },
+        left: {
+          color: '#fff'
+        },
+      }}
+
+    />
+    );
+  }
+
+  const rendersend=(props)=>{
+    return(
+      <Send {...props}>
+          <View>
+            <MaterialCommunity 
+            name='send-circle' 
+            size={32} 
+            color ='#9d4edd'
+            style={{
+            marginBottom: 5,
+            marginRight: 5,}}/> 
+          </View>
+      </Send>
+    );
+
+  }
+  const scrollToBottomComponent=()=>{
+    return(
+      <FontAwesome name='arrow-down' size={22} color='#333'/>
+    );
+  }
+
   return (
     <View style={globalStyles.container}>
-      <Text>Chat</Text>
-    </View>
-  );
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+      renderBubble={renderMyBubble}
+      alwaysShowSend
+      renderSend={rendersend}
+      scrollToBottom
+      scrollToBottomComponent={scrollToBottomComponent}
+    /> 
+    </View>)
+   
 }
--- front-end/Screens/Home.js
+++ front-end/Screens/Home.js
@@ -4,16 +4,31 @@ import { globalStyles } from '../styles/global';
 import { Button,NativeBaseProvider } from "native-base";
 
 export default function Home({navigation}) {
+  const username="Karim"
   const HandleClick=()=>
   {
     navigation.navigate("CreateListing")
   }
 
+  const HandleClickText=()=>
+  {
+    navigation.navigate("Chat",{UserName: username})
+  }
+
   return (
     <View style={globalStyles.container}>
       <NativeBaseProvider>
-      <Button onPress={HandleClick}>Create Trip</Button>
+      <Button style={styles.mybutton} onPress={HandleClick}>Create Trip</Button>
+      <Button onPress={HandleClickText}>Text me</Button>
       </NativeBaseProvider>
     </View>
   );
 }
+
+const styles=StyleSheet.create({
+  mybutton:{
+    margin: 10,
+
+  },
+})
+
